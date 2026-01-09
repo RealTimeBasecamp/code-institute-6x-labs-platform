7ea7c77a88a1c5ab74a3fec140cf2494e1f34d26
Rob Fraser <robertallanfraser@gmail.com>
2026-01-09 20:50:50 +0000

    fix: Simplify Cloudinary config to use CLOUDINARY_URL

--- saas_platform/settings.py
+++ saas_platform/settings.py
@@ -152,14 +152,8 @@ STORAGES = {
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary settings for media files (user uploads)
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-
+# CLOUDINARY_URL should be set in env.py and Heroku config vars
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
