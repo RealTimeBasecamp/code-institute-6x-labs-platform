c6bad4db556de63d92f79f213437ecf79d978cdd
Rob Fraser <robertallanfraser@gmail.com>
2026-01-10 20:29:15 +0000

    fix: Removed old sign up URL and view

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.login_view, name='login'),
-    path('sign-up/', views.sign_up_view, name='sign_up'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
