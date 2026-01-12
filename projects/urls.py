bdd3195f1c4d8dbc3f5a5f2d7971468d3f605e2e
Rob Fraser <robertallanfraser@gmail.com>
2026-01-12 12:53:30 +0000

    feat: Added create/delete project views/urls

--- projects/urls.py
+++ projects/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.ProjectListView.as_view(), name='projects_list'),
     path('project-planner/', views.project, name='project_planner'),
     path('project-planner/<slug:slug>/', views.project, name='project_planner_detail'),
+    path('project-planner/create-project/', views.create_project, name='create_project'),
+    path('project-planner/delete-project/<slug:slug>/', views.delete_project, name='delete_project'),
 ]
